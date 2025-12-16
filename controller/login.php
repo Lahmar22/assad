18ba636c0434e34aaa620684d5c15256a0a41a0c
Lahmar22 <lahmarzakariae22@gmail.com>
2025-12-16 20:59:59 +0100

    feat : update the function login for hash password

--- controller/login.php
+++ controller/login.php
@@ -20,7 +20,7 @@
             if ($result->num_rows === 1) {
                 $user = $result->fetch_assoc();
 
-                if ($password == $user['password']) {
+                if (password_verify($password, $user['password'])) {
                     
                     
                     $_SESSION['user_id'] = $user['id_user'];
