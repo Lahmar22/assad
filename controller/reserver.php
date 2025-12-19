83a6daa49c89085dd2be7a119f4c032ba9543e18
Lahmar22 <lahmarzakariae22@gmail.com>
2025-12-19 23:11:23 +0100

    feat : add the function for resever a  visite Guid by user

---
+++ controller/reserver.php
@@ -0,0 +1,18 @@
+<?php 
+require "../controller/connexion.php";
+
+$id_visiteGuid = $_POST['id_visiteGuid'];
+$id_user = $_POST['id_user'];
+$nbpersonnes = $_POST['nbpersonnes'];
+$datereservation = date("Y/m/d");
+
+$sqlReservation = "INSERT INTO reservations (idvisite, idutilisateur, nbpersonnes, datereservation) VALUES ('$id_visiteGuid', '$id_user ', '$nbpersonnes', '$datereservation')";
+
+if($conn->query($sqlReservation)=== true){
+    header("location: ../visiteur/home.php");
+
+} else {
+    echo "Erreur : " . $conn->error;
+}
+
+?>
