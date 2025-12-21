3c7ba055d862618a9de7ad60b84e49c76d150342
Lahmar22 <lahmarzakariae22@gmail.com>
2025-12-21 14:12:01 +0100

    feat : update function annuler a visite guid by visiteur

--- controller/annuler.php
+++ controller/annuler.php
@@ -2,18 +2,18 @@
 require "../controller/connexion.php";
 
 $id = $_POST["id"];
+$idvisite = $_POST["idvisite"];
+$nbrPersonne = $_POST["nbrPersonne"];
 
 
 $sql = "DELETE FROM reservations WHERE id = $id";
 
+$updatecapasiteVisite = "UPDATE visitesguidees SET capacite_max = capacite_max + $nbrPersonne WHERE id = $idvisite";
 
 
-if($conn->query($sql)=== true){
-        header("location: ../visiteur/home.php");
-        
-    } else {
-        echo "Erreur : " . $conn->error;
+if ($conn->query($sql) === true) {
+    $conn->query($updatecapasiteVisite);
+    header("location: ../visiteur/home.php");
+} else {
+    echo "Erreur : " . $conn->error;
 }
-
-
-?>
